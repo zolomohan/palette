4cb4c3b1010336a5513b2ac45d40c477a6163916
ZoloMohan <mohanzolomon@gmail.com>
2019-08-06 00:21:56 +0530

    Added Palette Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
+import Palette from './Palette';
 import './App.css';
+import seedColors from './seedColors';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        
+        <Palette {...seedColors[4]} /> 
       </div>
     );
   }
---
+++ src/Palette.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Palette extends Component {
+  render() {
+    return (
+      <div>
+				{/* Navbar */}
+				{/* Colors */}
+        {/* Footer */}
+      </div>
+    )
+  }
+}
