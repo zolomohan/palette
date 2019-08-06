9c99ee61072369fba263f527829a57c4cc593f07
ZoloMohan <mohanzolomon@gmail.com>
2019-08-06 21:50:23 +0530

    Added PaletteList Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 import Palette from './Palette';
+import PaletteList from './PaletteList'
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import seedColors from './seedColors';
 import {generatePalette} from './colorHelper'
-import { palette } from '@material-ui/system';
 
 class App extends Component {
 
@@ -13,7 +13,7 @@ class App extends Component {
   render(){
     return (
 			<Switch>
-				<Route exact path = '/' render = {() => <h1>Home Page</h1>} />
+				<Route exact path = '/' render = {() => <PaletteList palettes = {seedColors} />} />
 				<Route exact path = '/palette/:id' render = { routeProps => <Palette { ...generatePalette(this.findPalette(routeProps.match.params.id)) } /> } />
 			</Switch>
     );
