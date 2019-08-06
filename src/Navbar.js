e4c466a14334dd15d785860884be74afeceb2e45
ZoloMohan <mohanzolomon@gmail.com>
2019-08-06 16:00:35 +0530

    Added Navbar Component

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Navbar.css'
+
+export default class Navbar extends Component {
+	render() {
+		return (
+			<nav className='Navbar'>
+				<div className='Navbar-brand'>palette</div>
+			</nav>
+		)
+	}
+}
--- src/Palette.js
+++ src/Palette.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import ColorBox from './ColorBox'
+import Navbar from './Navbar'
 import './Palette.css'
 
 export default class Palette extends Component {
   render() {
     return (
       <div className='Palette'>
-				{/* Navbar */}
+				<Navbar />
 				<div className='Palette-colors'>
 					{this.props.colors[400].map(color => <ColorBox {...color} />)}
 				</div>
