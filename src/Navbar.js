9a9cf29bbab13e8b2328f845749dffe08da32d01
ZoloMohan <mohanzolomon@gmail.com>
2019-08-06 21:21:01 +0530

    Added Snackbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react'
 import Slider from '@material-ui/core/Slider';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 import './Navbar.css'
 
 export default class Navbar extends Component {
+
+	constructor(props) {
+		super(props)
+	
+		this.state = {
+			 snackbarOpen: false
+		}
+	}
+	
 
 	handleSliderChange = (event, level) => {
 		this.props.changeLevel(level);
@@ -12,6 +24,11 @@ export default class Navbar extends Component {
 
 	handleFormatChange = (event) => {
 		this.props.changeColorFormat(event.target.value);
+		this.setState({snackbarOpen: true});
+	}
+
+	closeSnackbar = () => {
+		this.setState({snackbarOpen: false})
 	}
 
 	render() {
@@ -30,11 +47,34 @@ export default class Navbar extends Component {
 				</div>
 				<div className='Navbar-select'>
 					<Select value = {this.props.format}  onChange = { this.handleFormatChange } >
-						<MenuItem value='hex'>Hex - #abcdef</MenuItem>
+						<MenuItem value='hex'>HEX - #abcdef</MenuItem>
 						<MenuItem value='rgb'>RGB - rgb(255, 255, 255)</MenuItem>
 						<MenuItem value='rgba'>RGBA - rgba(255, 255, 255, 1.0)</MenuItem>
 					</Select>
 				</div>
+				<Snackbar 
+					message = {
+						<span id="message-id">Format Changed to {this.props.format.toUpperCase()}</span>
+					}
+					anchorOrigin = {{
+						vertical: 'bottom',
+						horizontal: 'left'
+					}}
+					open = {this.state.snackbarOpen}
+					autoHideDuration = {3000}
+					ContentProps = {{
+						'aria-describedby': 'message-id',
+					}}
+					onClose = {this.closeSnackbar}
+					action = {[
+						<IconButton 
+							onClick = {this.closeSnackbar}
+							color='inherit'
+							key = 'Close Snackbar'>
+							<CloseIcon />
+						</IconButton>,
+					]}
+				/>
 			</nav>
 		)
 	}
