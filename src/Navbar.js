bb0ccb5e131c2cf54ca8c8c5eca24d0e90bf458f
ZoloMohan <mohanzolomon@gmail.com>
2019-08-06 16:34:28 +0530

    Added Slider Component to Change Shades of Colors

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
+import Slider from '@material-ui/core/Slider';
 import './Navbar.css'
 
 export default class Navbar extends Component {
+
+	handleSliderChange = (event, level) => {
+		this.props.changeLevel(level);
+	}
+
 	render() {
 		return (
 			<nav className='Navbar'>
 				<div className='Navbar-brand'>palette</div>
+				<div className='Navbar-slider'>
+					<span className='Navbar-slider-text'>{this.props.level}</span>
+					<Slider
+						defaultValue = {this.props.level}
+						step = {100}
+						min = {100}
+						max = {900}
+						onChange = {this.handleSliderChange}
+					/>
+				</div>
 			</nav>
 		)
 	}
--- src/Palette.js
+++ src/Palette.js
@@ -4,12 +4,20 @@ import Navbar from './Navbar'
 import './Palette.css'
 
 export default class Palette extends Component {
+
+	constructor(props) {
+		super();
+		this.state = { level: 400 }
+	}
+	
+	changeLevel = (level) => {this.setState({level})}
+
   render() {
     return (
       <div className='Palette'>
-				<Navbar />
+				<Navbar changeLevel = { this.changeLevel } level = { this.state.level } />
 				<div className='Palette-colors'>
-					{this.props.colors[400].map(color => <ColorBox {...color} />)}
+					{this.props.colors[this.state.level].map(color => <ColorBox {...color} />)}
 				</div>
         {/* Footer */}
       </div>
