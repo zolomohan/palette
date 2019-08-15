90961cec0fb318aae536c491dc24b3b3063a32ab
ZoloMohan <mohanzolomon@gmail.com>
2019-08-15 11:13:50 +0530

    Seperated JSS Styles from NewPaletteForm

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -10,65 +10,7 @@ import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-
-const drawerWidth = 240;
-
-const styles = (theme) => ({
-	root         : {
-		display : 'flex'
-	},
-	appBar       : {
-		transition : theme.transitions.create([ 'margin', 'width' ], {
-			easing   : theme.transitions.easing.sharp,
-			duration : theme.transitions.duration.leavingScreen
-		})
-	},
-	appBarShift  : {
-		width      : `calc(100% - ${drawerWidth}px)`,
-		marginLeft : drawerWidth,
-		transition : theme.transitions.create([ 'margin', 'width' ], {
-			easing   : theme.transitions.easing.easeOut,
-			duration : theme.transitions.duration.enteringScreen
-		})
-	},
-	menuButton   : {
-		marginLeft  : 12,
-		marginRight : 20
-	},
-	hide         : {
-		display : 'none'
-	},
-	drawer       : {
-		width      : drawerWidth,
-		flexShrink : 0
-	},
-	drawerPaper  : {
-		width : drawerWidth
-	},
-	drawerHeader : {
-		display        : 'flex',
-		alignItems     : 'center',
-		padding        : '0 8px',
-		...theme.mixins.toolbar,
-		justifyContent : 'flex-end'
-	},
-	content      : {
-		flexGrow   : 1,
-		padding    : theme.spacing.unit * 3,
-		transition : theme.transitions.create('margin', {
-			easing   : theme.transitions.easing.sharp,
-			duration : theme.transitions.duration.leavingScreen
-		}),
-		marginLeft : -drawerWidth
-	},
-	contentShift : {
-		transition : theme.transitions.create('margin', {
-			easing   : theme.transitions.easing.easeOut,
-			duration : theme.transitions.duration.enteringScreen
-		}),
-		marginLeft : 0
-	}
-});
+import styles from './styles/NewPaletteFormStyles';
 
 export default withStyles(styles, { withTheme: true })(
 	class NewPaletteForm extends Component {
