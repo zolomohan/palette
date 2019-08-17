ff5af819123be2342f4b09e7d93ab91d37f40b01
ZoloMohan <mohanzolomon@gmail.com>
2019-08-17 15:19:51 +0530

    Changed MenuIcon -> Add Circle in Create Palette Navbar

--- src/NewPaletteNavbar.js
+++ src/NewPaletteNavbar.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import clsx from 'clsx';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import AddCircle from '@material-ui/icons/AddCircle';
 import Button from '@material-ui/core/Button';
 import styles from './styles/NewPaletteNavbarStyles';
 import NewPaletteSaveDialog from './NewPaletteSaveDialog';
@@ -46,7 +45,7 @@ export default withStyles(styles)(
 							onClick={openDrawer}
 							className={clsx(classes.menuButton, open && classes.hide)}
 						>
-							<MenuIcon />
+							<AddCircle />
 						</IconButton>
 						<Typography variant='h6' color='inherit' noWrap>
 							New Palette
