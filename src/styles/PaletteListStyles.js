2dc4c27067c6e2651613627ccb995b2a7e8bb944
ZoloMohan <mohanzolomon@gmail.com>
2019-08-17 23:53:36 +0530

    Responsive PaletteList

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,22 +1,34 @@
-import background from './bg.svg'
+import background from './bg.svg';
+import sizes from '../helpers/sizes';
+
 export default {
 	root      : {
-		backgroundColor: '#3f3383',
-		backgroundImage: `url(${background})`,
-		backgroundSize: 'cover',
+		backgroundColor : '#3f3383',
+		backgroundImage : `url(${background})`,
+		backgroundSize  : 'cover',
 		display         : 'flex',
 		justifyContent  : 'center',
 		alignItems      : 'flex-start',
 		height          : '100vh',
-		overflowY: 'scroll',
+		overflowY       : 'scroll'
 	},
 	container : {
-		width         : '60%',
-		display       : 'flex',
-		alignItems    : 'flex-start',
-		flexDirection : 'column',
-		flexWrap      : 'wrap',
-		color         : 'white'
+		width              : '60%',
+		display            : 'flex',
+		alignItems         : 'flex-start',
+		flexDirection      : 'column',
+		flexWrap           : 'wrap',
+		color              : 'white',
+		transition         : '0.2s width linear',
+		[sizes.down('lg')]: {
+			width : '80%'
+		},
+		[sizes.down('md')]: {
+			width : '70%'
+		},
+		[sizes.down('sm')]: {
+			width : '80%'
+		}
 	},
 	nav       : {
 		display        : 'flex',
@@ -27,10 +39,10 @@ export default {
 		letterSpacing  : '1px',
 		textTransform  : 'uppercase',
 		'& a'          : {
-			textDecoration: 'none',
-			color : 'white',
-			'&:hover':{
-				color: '#dddddd',
+			textDecoration : 'none',
+			color          : 'white',
+			'&:hover'      : {
+				color : '#dddddd'
 			}
 		},
 		'& h1'         : {
@@ -41,7 +53,21 @@ export default {
 		display             : 'grid',
 		boxSizing           : 'border-box',
 		gridTemplateColumns : 'repeat(3, 30%)',
-		gridGap             : '5%',
-		width               : '100%'
+		gridGap             : '3rem',
+		width               : '100%',
+		paddingBottom: '5%',
+		[sizes.down('md')]: {
+			gridTemplateColumns : 'repeat(2, 50%)'
+		},
+		[sizes.down('sm')]: {
+			gridTemplateColumns : 'repeat(2, 50%)',
+			gridGap             : '2rem'
+		},
+		[sizes.down('xs')]: {
+			gridTemplateColumns : 'repeat(1, 100%)',
+			gridGap             : '1.5rem',
+			padding             : '10%',
+			paddingTop: '2%'
+		}
 	}
 };
