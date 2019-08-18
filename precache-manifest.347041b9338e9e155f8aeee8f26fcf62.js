eca5cd6b75abacf1b1239f6f7bf45de181c7515d
ZoloMohan <mohanzolomon@gmail.com>
2019-08-18 19:40:26 +0530

    Updates

---
+++ precache-manifest.347041b9338e9e155f8aeee8f26fcf62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8145eb25703bc55e6e81c78c8f8602ab",
+    "url": "/palette/index.html"
+  },
+  {
+    "revision": "67a2d5ce44cd6c517bdd",
+    "url": "/palette/static/css/2.b0f64131.chunk.css"
+  },
+  {
+    "revision": "1639dc03466aab24e3fe",
+    "url": "/palette/static/css/main.2d67b00f.chunk.css"
+  },
+  {
+    "revision": "67a2d5ce44cd6c517bdd",
+    "url": "/palette/static/js/2.90006961.chunk.js"
+  },
+  {
+    "revision": "1639dc03466aab24e3fe",
+    "url": "/palette/static/js/main.87a1c88e.chunk.js"
+  },
+  {
+    "revision": "8c991d8d8b4355bff54b",
+    "url": "/palette/static/js/runtime~main.67681100.js"
+  },
+  {
+    "revision": "794a28156503e6ffb8fda22caa16b5e8",
+    "url": "/palette/static/media/bg.794a2815.svg"
+  }
+]);
