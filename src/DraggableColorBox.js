2689b69c378a0b4717ba6ca67dcb93247d6c1e63
ZoloMohan <mohanzolomon@gmail.com>
2019-08-19 11:03:18 +0530

    Draggable Color Boxes -> PureComponent

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import { SortableElement } from 'react-sortable-hoc';
 import DeleteIcon from '@material-ui/icons/Delete';
 import styles from './styles/DraggableColorBoxStyles';
 
 export default SortableElement(
-	withStyles(styles)(function DraggableColorBox(props) {
-		const { root, boxContent, deleteIcon, colorName } = props.classes;
-		function handleDeleteColor() {
-			props.deleteColor(props.color);
+	withStyles(styles)(
+		class DraggableColorBox extends PureComponent {
+			handleDeleteColor = () => {
+				this.props.deleteColor(this.props.color);
+			};
+
+			render() {
+				const { root, boxContent, deleteIcon, colorName } = this.props.classes;
+				console.log('Rendering');
+				return (
+					<div className={root}>
+						<div className={boxContent}>
+							<span className={colorName}>{this.props.color.name}</span>
+							<DeleteIcon className={deleteIcon} onClick={this.handleDeleteColor} />
+						</div>
+					</div>
+				);
+			}
 		}
-		return (
-			<div className={root}>
-				<div className={boxContent}>
-					<span className={colorName}>{props.color.name}</span>
-					<DeleteIcon className={deleteIcon} onClick={handleDeleteColor} />
-				</div>
-			</div>
-		);
-	})
+	)
 );
