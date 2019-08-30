a625740234e7b6591dd329a5bf617340f3197bbd
ZoloMohan <mohanzolomon@gmail.com>
2019-08-30 07:47:30 +0530

    Refactor /helpers/sizes.js

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,1 +1,8 @@
-export const DRAWER_WIDTH = 300;
+export const 
+	DRAWER_WIDTH = 300,
+  MEDIA_BREAKPOINTS = {
+		xs : '575.98px',
+		sm : '575.98px',
+		md : '991.98px',
+		lg : '1199.98px'
+	};
--- src/helpers/sizes.js
+++ src/helpers/sizes.js
@@ -1,20 +1,6 @@
+import { MEDIA_BREAKPOINTS } from './constants';
+
 export default {
-	up(size){
-		const breakpoints = {
-			xs: '575.98px',
-			sm: '767.98px',
-			md: '991.98px',
-			lg: '1199.98px'
-		}
-		return `@media(min-width:${breakpoints[size]})`		
-	},
-	down(size){
-		const breakpoints = {
-			xs: '575.98px',
-			sm: '767.98px',
-			md: '991.98px',
-			lg: '1199.98px'
-		}
-		return `@media(max-width:${breakpoints[size]})`
-	}
-}
+	up   : (size) => `@media(min-width:${MEDIA_BREAKPOINTS[size]})`,
+	down : (size) => `@media(max-width:${MEDIA_BREAKPOINTS[size]})`
+};
