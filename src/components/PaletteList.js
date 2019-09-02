9aaaafb9e9065ace3b26fb2de00ac0955413ba9e
ZoloMohan <mohanzolomon@gmail.com>
2019-09-02 11:37:59 +0530

    Convert PaletteList.js to Functional Component

--- src/components/PaletteList.js
+++ src/components/PaletteList.js
@@ -1,32 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/styles';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import MiniPalette from './MiniPalette';
 import styles from '../styles/PaletteListStyles';
 
-export default withStyles(styles)(
-	class PaletteList extends Component {
-		render() {
-			const { classes, palettes, deletePalette } = this.props;
-			const { root, container, nav } = classes;
-			return (
-				<div className={root}>
-					<div className={container}>
-						<nav className={nav}>
-							<h1>Palette</h1>
-							<Link to={`${process.env.PUBLIC_URL}/palette/new`}>Create Palette</Link>
-						</nav>
-						<TransitionGroup className={classes.palettes}>
-							{palettes.map((palette) => (
-								<CSSTransition key={palette.id} classNames='fade' timeout={500}>
-									<MiniPalette {...palette} key={palette.id} deletePalette={deletePalette} />
-								</CSSTransition>
-							))}
-						</TransitionGroup>
-					</div>
-				</div>
-			);
-		}
-	}
-);
+export default withStyles(styles)(function PaletteList({ classes, palettes, deletePalette }) {
+	return (
+		<div className={classes.root}>
+			<div className={classes.container}>
+				<nav className={classes.nav}>
+					<h1>Palette</h1>
+					<Link to={`${process.env.PUBLIC_URL}/palette/new`}>Create Palette</Link>
+				</nav>
+				<TransitionGroup className={classes.palettes}>
+					{palettes.map((palette) => (
+						<CSSTransition key={palette.id} classNames='fade' timeout={500}>
+							<MiniPalette {...palette} key={palette.id} deletePalette={deletePalette} />
+						</CSSTransition>
+					))}
+				</TransitionGroup>
+			</div>
+		</div>
+	);
+});
