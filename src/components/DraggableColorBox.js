399435c728a0a349f047555aa55c7b9010b9d387
ZoloMohan <mohanzolomon@gmail.com>
2019-09-02 12:00:47 +0530

    Convert DraggableColorBox to Functional Component

--- src/components/DraggableColorBox.js
+++ src/components/DraggableColorBox.js
@@ -1,27 +1,21 @@
-import React, { PureComponent } from 'react';
+import React, { memo } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import { SortableElement } from 'react-sortable-hoc';
 import DeleteIcon from '@material-ui/icons/Delete';
 import styles from '../styles/DraggableColorBoxStyles';
 
 export default SortableElement(
-	withStyles(styles)(
-		class DraggableColorBox extends PureComponent {
-			handleDeleteColor = () => {
-				this.props.deleteColor(this.props.color);
-			};
-
-			render() {
-				const { root, boxContent, deleteIcon, colorName } = this.props.classes;
-				return (
-					<div className={root}>
-						<div className={boxContent}>
-							<span className={colorName}>{this.props.color.name}</span>
-							<DeleteIcon className={deleteIcon} onClick={this.handleDeleteColor} />
-						</div>
+	memo(
+		withStyles(styles)(function DraggableColorBox({ deleteColor, classes, color }) {
+			const { root, boxContent, deleteIcon, colorName } = classes;
+			return (
+				<div className={root}>
+					<div className={boxContent}>
+						<span className={colorName}>{color.name}</span>
+						<DeleteIcon className={deleteIcon} onClick={() => deleteColor(color)} />
 					</div>
-				);
-			}
-		}
+				</div>
+			);
+		})
 	)
 );
