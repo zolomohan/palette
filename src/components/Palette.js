d984ae838e39411ab671069aa37079183eb08358
ZoloMohan <mohanzolomon@gmail.com>
2019-09-02 11:35:03 +0530

    Convert Palette.js to Functional Component

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -1,37 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { withStyles } from '@material-ui/styles';
 import ColorBox from './ColorBox';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import styles from '../styles/PaletteStyles';
 
-export default withStyles(styles)(
-	class Palette extends Component {
-		state = {
-			level  : 400,
-			format : 'hex'
-		};
+export default withStyles(styles)(function Palette({ classes, colors, id, emoji, paletteName }) {
+	const [ level, setLevel ] = useState(400);
+	const [ format, setFormat ] = useState('hex');
 
-		changeLevel = (level) => this.setState({ level });
-
-		changeColorFormat = (format) => this.setState({ format });
-
-		render() {
-			const { changeColorFormat, changeLevel, props, state } = this;
-			const { classes, colors, id, emoji, paletteName } = props;
-			const { level, format } = state;
-			const { palette, paletteColors } = classes;
-			return (
-				<div className={palette}>
-					<Navbar changeLevel={changeLevel} changeColorFormat={changeColorFormat} level={level} format={format} />
-					<div className={paletteColors}>
-						{colors[level].map((color) => (
-							<ColorBox {...color} format={format} key={color.id} paletteId={id} singleColorPalette={false} />
-						))}
-					</div>
-					<Footer paletteName={paletteName} emoji={emoji} />
-				</div>
-			);
-		}
-	}
-);
+	const changeLevel = (level) => setLevel(level),
+		changeColorFormat = (format) => setFormat(format);
+	return (
+		<div className={classes.palette}>
+			<Navbar changeLevel={changeLevel} changeColorFormat={changeColorFormat} level={level} format={format} />
+			<div className={classes.paletteColors}>
+				{colors[level].map((color) => (
+					<ColorBox {...color} format={format} key={color.id} paletteId={id} singleColorPalette={false} />
+				))}
+			</div>
+			<Footer paletteName={paletteName} emoji={emoji} />
+		</div>
+	);
+});
