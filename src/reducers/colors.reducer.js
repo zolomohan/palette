e92c9b091ccca436e34aed6e445a7cddbb78bb0b
ZoloMohan <mohanzolomon@gmail.com>
2019-09-06 01:26:48 +0530

    Add colors.reducer for CreatePalette State

---
+++ src/reducers/colors.reducer.js
@@ -0,0 +1,16 @@
+import arrayMove from 'array-move';
+
+export default (state, action) => {
+	switch(action.type) {
+		case 'ADD':
+			return [ ...state, action.color ];
+		case 'DELETE':
+			return state.filter((color) => color.color !== action.color);
+		case 'CLEAR': 
+			return [];
+		case 'SORT':
+			return arrayMove(state, action.oldIndex, action.newIndex)
+		default: 
+			return state;
+	}
+}
