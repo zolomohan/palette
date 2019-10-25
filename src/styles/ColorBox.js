c278d90404142b2894c11f046daaad0cc543a690
Mohan Raj <mohanzolomon@gmail.com>
2019-10-25 13:42:04 +0530

    Variable ColorBox Height at lg and xl

--- src/styles/ColorBox.js
+++ src/styles/ColorBox.js
@@ -7,11 +7,14 @@ const colorBoxHeight = (length, media, singleColorShades) => {
 		if (media === 'md') return '20%';
 		if (media === 'xs') return '10%';
 		return '50%';
-  }
-  
+	}
 	if (media === 'xs') return `${100 / length}%`;
-  if (media === 'md') return `${100 / (length % 2 === 0 ? length : length + 1) * 2}%`;
-  return '25%';
+	if (media === 'md') return `${100 / (length % 2 === 0 ? length : length + 1) * 2}%`;
+	if (media === 'lg')
+		return `${100 /
+			(length > 16 ? 5 : length > 12 ? 4 : length > 8 ? 3 : length > 4 ? 2 : 1)}%`;
+	if (media === 'xl')
+		return `${100 / (length > 15 ? 4 : length > 10 ? 3 : length > 5 ? 2 : 1)}%`;
 };
 
 export default {
